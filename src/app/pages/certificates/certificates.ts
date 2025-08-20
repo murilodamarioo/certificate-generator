e358433dfea00b8f21491f676c445175854e9cba
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-20 15:30:19 -0300

    :sparkles: feat: Implements list certificates

--- src/app/pages/certificates/certificates.ts
+++ src/app/pages/certificates/certificates.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+import { CertificateItem } from "../../_components/certificate-item/certificate-item";
 
 @Component({
   selector: 'app-certificates',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, CertificateItem],
   templateUrl: './certificates.html',
   styleUrl: './certificates.css'
 })
