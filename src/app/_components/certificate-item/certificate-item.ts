9a2bdf02f6546953962881f28b089f37556520f2
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-20 13:49:04 -0300

    :sparkles: feat: Implements Certificate Item  component

---
+++ src/app/_components/certificate-item/certificate-item.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from "../secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-certificate-item',
+  imports: [SecondaryButton],
+  templateUrl: './certificate-item.html',
+  styleUrl: './certificate-item.css'
+})
+export class CertificateItem {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { CertificateItem } from "./_components/certificate-item/certificate-item";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, CertificateItem],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
