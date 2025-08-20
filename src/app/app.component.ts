7e78dd72f0b637aff4a419eca0fa7b288bd36243
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-20 14:19:45 -0300

    :sparkles: feat: Implements Base UI component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { Navbar } from "./_components/navbar/navbar";
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 import { CertificateItem } from "./_components/certificate-item/certificate-item";
+import { BaseUi } from "./_components/base-ui/base-ui";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, CertificateItem],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, CertificateItem, BaseUi],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
