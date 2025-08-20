70321f759624e0218369b7ec25a27aa535bdb678
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-20 13:35:10 -0300

    :sparkles: feat: Implements Secondary Button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
