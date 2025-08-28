e122619daa7ac4edc1a2f026252ce58a72115c09
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-28 11:10:43 -0300

    :sparkles: feat: Using NgModel in the form

--- src/app/pages/form-certificate/form-certificate.ts
+++ src/app/pages/form-certificate/form-certificate.ts
@@ -1,13 +1,17 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
 
 @Component({
   selector: 'app-form-certificate',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule],
   templateUrl: './form-certificate.html',
   styleUrl: './form-certificate.css'
 })
 export class FormCertificate {
-
+  name: string = ''
+  activity: string = ''
+  activities: string[] = ['Angular', 'React']
 }
