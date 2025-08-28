71dc3a9647ed8b31b05aff4a6a551ddd3ca8028e
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-28 15:28:05 -0300

    :sparkles: feat: Creates Certificate interface

--- src/app/pages/form-certificate/form-certificate.ts
+++ src/app/pages/form-certificate/form-certificate.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { Certificate } from '../../interfaces/certificate';
 
 
 @Component({
@@ -13,23 +14,34 @@ import { PrimaryButton } from "../../_components/primary-button/primary-button";
   styleUrl: './form-certificate.css'
 })
 export class FormCertificate {
-  name: string = ''
   activity: string = ''
-  activities: string[] = []
+
+  certificate: Certificate = {
+    name: '',
+    activities: []
+  }
 
   invalidField(control: NgModel) {
     return control.invalid && control.touched
   }
 
   validForm() {
-    return this.activities.length > 0 && this.name.length > 0
+    return this.certificate.activities.length > 0 && this.certificate.name.length > 0
   }
   addActivity() {
-    this.activities.push(this.activity)
+    this.certificate.activities.push(this.activity)
     this.activity = ''
   }
 
   deleteActivity(index: number) {
-    this.activities.splice(index, 1)
+    this.certificate.activities.splice(index, 1)
+  }
+
+  submit() {
+    if (!this.validForm()) {
+      return
+    }
+
+    console.log(this.certificate)
   }
 }
